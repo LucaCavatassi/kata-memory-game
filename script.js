f2eeaefad2a1e4c74f38696d06456646649243d7
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-02-03 17:46:55 +0100

    feat: restart screen and winning screen

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const elements = {
     spaceship: { shown: false, imgUrl: './assets/images/spaceship.png' },
     tiktac: { shown: false, imgUrl: './assets/images/tiktac.png' },
 };
-// Classnames to assing
+// Classnames to assign
 const classNames = ['alien', 'bug', 'duck', 'rocket', 'spaceship', 'tiktac', 'alien', 'bug', 'duck', 'rocket', 'spaceship', 'tiktac'];
 // Shuffle randomness
 classNames.sort(() => Math.random() - 0.5);
@@ -15,11 +15,12 @@ classNames.sort(() => Math.random() - 0.5);
 // Array to compare couples
 let selectedItems = [];
 
-// Error counter and html relative
+// Error counter and HTML reference
 let errorCounter = 0;
-let errorHtml = document.getElementById('errorNumber')
+let errorHtml = document.getElementById('errorNumber');
 errorHtml.innerText = errorCounter;
-// Pair founded counter
+
+// Pair found counter
 let pairCounter = 0;
 
 // Img List
@@ -29,38 +30,62 @@ const listItems = imgList.querySelectorAll('li');
 
 listItems.forEach((elem, index) => {
     const className = classNames[index]; // Get assigned class
-    elem.classList.add(className) //Add class to elem
+    elem.classList.add(className); // Add class to elem
 
-    elem.addEventListener("click", (e) => {
+    elem.addEventListener("click", () => {
+        if (selectedItems.length === 2) return; // Prevent more than 2 selections
+
         let elemClass = elem.classList[0]; // Get the first class only
-        const img = elem.querySelector('img') //Get the image tag inside the li
+        const img = elem.querySelector('img'); // Get the image tag inside the li
 
         img.src = elements[elemClass].imgUrl; // Assign the corresponding imgUrl
-        selectedItems.push({elem, elemClass})  // Push the classname added to the selected items, so can track the clicks
-        
-        if (selectedItems.length === 2) { // If array has length of 2 so there are two elements 
-            if (selectedItems[0].elemClass === selectedItems[1].elemClass) { // Confront them if they are equal match found go on
-                console.log('match found');
-                pairCounter++
+        selectedItems.push({ elem, elemClass }); // Track the clicked elements
+
+        if (selectedItems.length === 2) { // If two elements are selected
+            disableClicks(); // Disable clicking
+
+            if (selectedItems[0].elemClass === selectedItems[1].elemClass) { // If match found
+                pairCounter++;
                 if (pairCounter === 6) {
-                    console.log('You win');
+                    document.getElementById('grid').style.display = 'none'
+                    document.getElementById('win-screen').style.display = 'block'
                 }
                 selectedItems = [];
-            } else { // if not hide them after 1 second
-                // Add 1 to error counter and show it
-                errorCounter++
+                enableClicks(); // Re-enable clicking immediately
+            } else { // If not a match
+                errorCounter++;
                 errorHtml.innerText = errorCounter;
 
                 setTimeout(() => {
-                    selectedItems.forEach((elem) => {
-                        
-                        let img = elem.elem.querySelector('img') //Get the image tag inside the li
-                        img.src = './assets/images/back.png' //Return to hide
-                    })
-                    selectedItems = []; //Empty the array
-                }, 1000)
+                    selectedItems.forEach(({ elem }) => {
+                        let img = elem.querySelector('img'); // Get the image tag inside the li
+                        img.src = './assets/images/back.png'; // Hide again
+                    });
+                    selectedItems = [];
+                    enableClicks(); // Re-enable clicking after hiding
+                }, 1000);
             }
         }
-        
+    });
+});
+
+// Function to disable clicks on all list items
+function disableClicks() {
+    listItems.forEach((li) => li.style.pointerEvents = "none");
+}
+
+// Function to re-enable clicks on all list items
+function enableClicks() {
+    listItems.forEach((li) => li.style.pointerEvents = "auto");
+}
+
+// Reload button
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("refreshBtn").addEventListener("click", () => {
+        const winScreen = document.getElementById('win-screen');
+        if (winScreen) {
+            winScreen.style.display = 'none';
+        }
+        location.reload();
     });
 });
