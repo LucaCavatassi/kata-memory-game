7824b9ed58cf2c90458da1eb2d9fe05fc491d5d2
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-02-03 16:09:25 +0100

    feat: basic grid no interaction

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-console.log('hello');
-
 const elements = {
-    alien: {shown: false, imgUrl: './assets/images/alien.png'}
+    alien: {shown: false, imgUrl: './assets/images/alien.png'},
+    back: {shown: false, imgUrl: './assets/images/alien.png'},
+    bug: {shown: false, imgUrl: './assets/images/alien.png'},
+    duck: {shown: false, imgUrl: './assets/images/alien.png'},
+    rocket: {shown: false, imgUrl: './assets/images/alien.png'},
+    spaceship: {shown: false, imgUrl: './assets/images/alien.png'},
+    tiktac: {shown: false, imgUrl: './assets/images/alien.png'},
+    white: {shown: false, imgUrl: './assets/images/alien.png'},
 }
+
+Object.entries(elements).forEach((element) => {
+    console.log(element[1].shown)
+});
