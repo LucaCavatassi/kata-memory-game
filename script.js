694fbeb6f895efb1140c7cdaae9e6d2f55153ddb
LucaCavatassi <lucamariacavatassi@gmail.com>
2025-02-03 16:53:54 +0100

    feat: dynamical assign of images

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
+// Dictionary
 const elements = {
-    alien: {shown: false, imgUrl: './assets/images/alien.png'},
-    back: {shown: false, imgUrl: './assets/images/alien.png'},
-    bug: {shown: false, imgUrl: './assets/images/alien.png'},
-    duck: {shown: false, imgUrl: './assets/images/alien.png'},
-    rocket: {shown: false, imgUrl: './assets/images/alien.png'},
-    spaceship: {shown: false, imgUrl: './assets/images/alien.png'},
-    tiktac: {shown: false, imgUrl: './assets/images/alien.png'},
-    white: {shown: false, imgUrl: './assets/images/alien.png'},
-}
+    alien: { shown: false, imgUrl: './assets/images/alien.png' },
+    bug: { shown: false, imgUrl: './assets/images/bug.png' },
+    duck: { shown: false, imgUrl: './assets/images/duck.png' },
+    rocket: { shown: false, imgUrl: './assets/images/rocket.png' },
+    spaceship: { shown: false, imgUrl: './assets/images/spaceship.png' },
+    tiktac: { shown: false, imgUrl: './assets/images/tiktac.png' },
+};
+// Classnames to assing
+const classNames = ['alien', 'bug', 'duck', 'rocket', 'spaceship', 'tiktac', 'alien', 'bug', 'duck', 'rocket', 'spaceship', 'tiktac'];
 
-Object.entries(elements).forEach((element) => {
-    // console.log(element[1].shown)
-});
-
+// Shuffle randomness
+classNames.sort(() => Math.random() - 0.5);
 
 const imgList = document.getElementById('imgList');
 const listItems = imgList.querySelectorAll('li');
 
 listItems.forEach((elem, index) => {
-    elem.setAttribute('id', `item-${index + 1}`);
+    const className = classNames[index]; // Get assigned class
+    elem.classList.add(className) //Add class to elem
+
     elem.addEventListener("click", (e) => {
-        console.log(elem.id);
-        const img = elem.querySelector('img');
-        img.src = elements.alien.imgUrl;
-        
+        let elemClass = elem.classList[0]; // Get the first class only
+        const img = elem.querySelector('img'); // If elements has that key assign the corresponding imgUrl
+        img.src = elements[elemClass].imgUrl;
     });
 });
